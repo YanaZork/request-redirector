975980260ed348b086393f89f3e383450cc77719
Zork <yana_zorkaya2@mail.ru>
2021-12-08 11:33:28 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+//import './App.css';
+import { DataGrid } from '@mui/x-data-grid';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
 import Button from '@mui/material/Button';
+import { render } from 'sass';
+import Item from 'antd/lib/list/Item';
 
-function App() {
+
+export default function BasicDatePicker() {
+  const [valueStart, setValueStart] = React.useState(null);
+  const [valueEnd, setValueEnd] = React.useState(null);
+
+  //const [ currentID, setCurrentID ] = useState(1);
+
+function request() {
+  fetch("http://localhost:5000/", {
+    mode: 'no-cors',
+    //method: "post",
+  })
+  .then(res => console.log(res))
+  .then(data => console.log(data));
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained">Hello World</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DatePicker
+          label="Start"
+          value={valueStart}
+          onChange={(newValue) => {
+            setValueStart(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        <DatePicker
+          label="End"
+          value={valueEnd}
+          onChange={(newValue) => {
+            setValueEnd(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        <Button variant="contained" onClick={request()}>Получить</Button>
+      </LocalizationProvider>
+
+
     </div>
   );
 }
-
-export default App;
